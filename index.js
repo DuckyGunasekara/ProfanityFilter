0efbe690da12d91c58d6ec9d9734a9d9090b26aa
Daksitha <duksgun@gmail.com>
2018-04-05 09:45:26 +0200

    Partially working filter with includion of (mostly) complete list of profane words by google

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var filter = require("leo-profanity");
+console.log(filter.list());
+console.log(filter.clean('I have assassin ass'));
---
+++ index.ts
@@ -0,0 +1,7 @@
+//import * as fs from 'fs';
+import * as path from 'path';
+import * as filter from 'leo-profanity';
+
+console.log(filter.list());
+
+console.log(filter.clean('I have assassin ass'));
